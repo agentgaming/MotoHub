ef7c499114c98afa4bae328a157531143b7b4b39
Dakota Baber <dakota@dasigns.net>
2013-08-31 16:38:35 -0400

    Play rigby on login (temporary)

--- src/main/java/net/agentgaming/motohub/MotoHub.java
+++ src/main/java/net/agentgaming/motohub/MotoHub.java
@@ -1,5 +1,6 @@
 package net.agentgaming.motohub;
 
+import com.mike724.motoapi.midi.MidiBroadcaster;
 import com.mike724.motoapi.portals.PortalManager;
 import com.mike724.motoserver.MotoCommands;
 import com.mike724.motoserver.MotoServer;
@@ -9,6 +10,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.sound.midi.MidiUnavailableException;
 
 public class MotoHub extends JavaPlugin {
 
@@ -20,21 +23,35 @@ public class MotoHub extends JavaPlugin {
     //World spawn
     Location worldSpawn;
 
+    //MidiBroadcaster
+    MidiBroadcaster midi;
+
     @Override
     public void onEnable() {
         //Set our instance
         instance = this;
 
+        //Setup sync tasks
         Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new TimeDay(), 1l, 1l);
         Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new PotionSetter(), 1l, 20l);
 
+        //Register events
         this.getServer().getPluginManager().registerEvents(new HubListener(), this);
 
+        //Setup portals
         setupPortals();
 
+        //Setup spawn command
         this.worldSpawn = new Location(Bukkit.getWorlds().get(0), 0.5F, 65, 0.5F, 0, 0);
-
         getCommand("spawn").setExecutor(new HubCommands(this));
+
+        //TODO: Temporary
+        try {
+            midi = new MidiBroadcaster();
+        } catch (MidiUnavailableException e) {
+            e.printStackTrace();
+        }
+
 
         this.getLogger().info("MotoHub Enabled");
     }
@@ -54,6 +71,10 @@ public class MotoHub extends JavaPlugin {
 
     public Location getWorldSpawn() {
         return worldSpawn;
+    }
+
+    public MidiBroadcaster getMidi() {
+        return midi;
     }
 
     @SuppressWarnings("unused")
--- src/main/java/net/agentgaming/motohub/events/HubListener.java
+++ src/main/java/net/agentgaming/motohub/events/HubListener.java
@@ -42,6 +42,7 @@ public class HubListener implements Listener {
     public void onPlayerJoinMonitor(PlayerJoinEvent event) {
         event.setJoinMessage(ChatColor.YELLOW + event.getPlayer().getDisplayName() + " has joined the hub");
         event.getPlayer().setGameMode(GameMode.ADVENTURE);
+        MotoHub.getInstance().getMidi().playMidiForPlayer("rigby", event.getPlayer());
     }
 
     @EventHandler(priority = EventPriority.HIGH)
@@ -107,8 +108,6 @@ public class HubListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onJoin(PlayerJoinEvent event) {
-        if (!event.getPlayer().hasPlayedBefore()) {
-            event.getPlayer().teleport(MotoHub.getInstance().getWorldSpawn());
-        }
+        event.getPlayer().teleport(MotoHub.getInstance().getWorldSpawn());
     }
 }
