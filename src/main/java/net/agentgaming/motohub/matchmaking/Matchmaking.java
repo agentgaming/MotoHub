501c9eec8bf4f39e6b2e978fb97d344e6eee417c
Dakota Baber <dakota@dasigns.net>
2013-09-02 23:19:11 -0400

    Longer delay..

--- src/main/java/net/agentgaming/motohub/matchmaking/Matchmaking.java
+++ src/main/java/net/agentgaming/motohub/matchmaking/Matchmaking.java
@@ -140,7 +140,7 @@ public class Matchmaking {
                     mp.cmd("pc", pName);
                 }
             }
-        }, 2);
+        }, 20);
     }
 
     public void joinGroup(Player leader, Player member) {
